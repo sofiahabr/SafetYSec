480c50a6d1c29cf9bf0c046615cd42986cdff6fb
Sofia Håbrekke <sofiash@stud.ntnu.no>
2025-11-15 12:58:47 +0100

    Set up navigation and basic pages

--- app/src/main/java/com/example/safetysec/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/safetysec/presentation/screens/home/HomeScreen.kt
@@ -1,2 +1,34 @@
 package com.example.safetysec.presentation.screens.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.safetysec.navigation.AppRoutes
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column (
+
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Home Screen")
+        Button(onClick = {
+            navController.navigate(AppRoutes.PROFILE)
+        }) {
+            Text("Go to profile")
+        }
+        Button(onClick = {
+            navController.navigate(AppRoutes.LOGIN)
+        }) {
+            Text("Go to login")
+        }
+    }
+}
