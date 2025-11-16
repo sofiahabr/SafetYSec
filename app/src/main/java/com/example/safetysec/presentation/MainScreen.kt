4225fb4da00de7efe3c8dd400203384f37a9216f
Josefine Arntsen <josefia@ulrik.uio.no>
2025-11-16 23:45:50 +0000

    fixed package

--- app/src/main/java/com/example/safetysec/presentation/MainScreen.kt
+++ app/src/main/java/com/example/safetysec/presentation/MainScreen.kt
@@ -1,4 +1,4 @@
-package com.safetysec.presentation
+package com.example.safetysec.presentation
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
